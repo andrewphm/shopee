fcfe1a34007f85912c8abf739f1d343c3f1c5c61
andrewphm <andrpham@gmail.com>
2021-11-26 14:01:12 -0500

    Create mongodb schemas for Cart, Order, Product and Users

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const userRoute = require('./routes/user');
+const { application } = require('express');
 
 dotenv.config();
 
@@ -11,4 +14,10 @@ mongoose
   .catch((err) => console.error(err));
 
 const app = express();
-app.listen(5000, () => console.log('Server started'));
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userRoute);
+
+app.listen(process.env.PORT || 5000, () => console.log('Server started'));
