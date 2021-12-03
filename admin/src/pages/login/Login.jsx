bfe553fd7f8a3f7020ca6156e699f26de88c1318
andrewphm <andrpham@gmail.com>
2021-12-03 14:03:56 -0500

    Add admin portal

---
+++ admin/src/pages/login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <input type="text" placeholder="username"></input>
+      <input type="password" placeholder="password"></input>
+    </div>
+  );
+};
+
+export default Login;
